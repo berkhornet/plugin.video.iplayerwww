6e3a4ac4b204c8be127395fb19b99629a3510db3
berkhornet <67786207+berkhornet@users.noreply.github.com>
2026-02-28 23:09:02 +0000

    Revert "Tweak BBC-004"
    This reverts commit 1c05e546d3620d8eb267a775e9ba1ae04efcd5af.

--- resources/lib/ipwww_video.py
+++ resources/lib/ipwww_video.py
@@ -936,9 +936,8 @@ def ParseProgramme(progr_data, playable=False):
     else:
         programme = {
             'url': 'https://www.bbc.co.uk/iplayer/episodes/' + progr_data['id'],
-            # BBC-004: remove "x episodes available" from title
-            # 'name': '[B]{}[/B] - {} episodes available'.format(progr_data['title'], progr_data['count'])
-            'name': '{}'.format(progr_data['title'])
+            'name': '[B]{}[/B] - {} episodes available'.format(progr_data['title'], progr_data['count'])
+        }
 
     programme.update({
         'iconimage': progr_data.get('images', {}).get('standard', 'DefaultFolder.png').replace('{recipe}', '832x468'),
